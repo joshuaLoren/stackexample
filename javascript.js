e59a5f9fa689de038c80415e590b5080f0daace0
Joshua <smallj3@outlook.com>
2018-05-25 17:51:07 -0400

    finished debugging

--- javascript.js
+++ javascript.js
@@ -6,39 +6,21 @@ var isEmptyButton = document.getElementById("isEmpty");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var list = document.querySelectorAll("li");
-var div = document.querySelector("div");//.getElementsByTagName("li");
-var contentToReturn;
+var div = document.querySelector("div");
+var innerHTMl;																					//holds text of the next element to pop
 var count = 0;
-//div now contains all <li> elements in itself, even the ones that haven't been added yet...
-
-/*function deleteThisButton(e){
-	e.target.parentNode.remove();
-}*/
-
-
-function pop(e){
-  contentToReturn = e.target.parentNode.textContent;
-	e.target.parentNode.remove();
-  count--;
-  alert("You popped " + contentToReturn);
-}
+var numberOfNodes = 0;
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
-	//var li = document.createElement("li");
-  //li.id = "listItem";
 	var deleteButton = document.createElement("BUTTON");
 	deleteButton.innerHTML = input.value;
 	deleteButton.id = "deleteButton";
-
   ul.appendChild(deleteButton);
-	//li.appendChild(deleteButton);
 	input.value = "";
-
-	/*li.appendChild(document.createTextNode(input.value));*/
   if(count == 0){
 	  ul.appendChild(deleteButton);
 	  input.value = "";
@@ -46,39 +28,61 @@ function createListElement() {
   else{
     ul.insertBefore(deleteButton, ul.childNodes[0]);
   }
-
 }
 
+/**
+  When the user selects to push to the Stack
+*/
 function addListAfterClick() {
-	if (inputLength() > 0 && count <= 5) {
-		createListElement();
-    count++;
-  if(count >= 5){
-  alert("This stack can only hold five elements");
+	  if (inputLength() > 0 && count < 5) {
+		  createListElement();
+      count++;
+    if(count >= 5){
+    alert("This is the last element you'll be able to add as this stack can only hold five elements");
 	}
-}
+ }
 }
 
+/**
+ addListAfterKeypress allows the user to push items pushing the enter key
+*/
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13 && count <= 5) {
-		createListElement();
-    count++;
+	   createListElement();
+     count++;
   if(count >= 5){
-    alert("This stack can only hold five elements");
+     alert("This stack can only hold five elements");
   }
-	}
+ }
 }
+
+/*popButton.addEventListener("click", function(){
+  if(count == 1){
+    alert("Popping...removing " + deleteButton.innerText);
+    deleteButton.remove();
+  }
+	else{
+  deleteButton[0].remove();
+  alert("Popping...removing " + deleteButton.innerText);
+  count--;
+  }
+});*/
 
 
 popButton.addEventListener("click", function(){
-  if(count == 1)
-  {
-    alert("Popping...removing " + deleteButton.innerHTML);
-    deleteButton.remove();
+  if(count == 1){
+		innerHTML = deleteButton.innerHTML;
+		deleteButton.remove();
+    alert("Popping...the value at the top of the stack is: " + innerHTML);
+		count--;
   }
-  deleteButton[0].remove();
-  alert("Popping...removing " + deleteButton.innerHTML);
-  count--;
+	else{
+		innerHTML = deleteButton[0].innerHTML;
+		deleteButton[0].remove();
+  	alert("Popping...the value at the top of the stack is: " + innerHTML);
+		count--;
+  }
+
 });
 
 peekButton.addEventListener("click", function(){
@@ -97,6 +101,5 @@ isEmpty.addEventListener("click", function(){
   }
 });
 
-ul.addEventListener("click", pop);
 pushButton.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
